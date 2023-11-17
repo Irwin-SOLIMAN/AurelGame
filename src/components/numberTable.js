2c5f2603a2abde603aea76c5f5796f0a7cf9a229
Irwin <irwin.soliman@gmail.com>
2023-11-17 15:16:05 +0100

    publication

--- src/components/numberTable.js
+++ src/components/numberTable.js
@@ -1,62 +1,62 @@
 const numberTable = [
     {
         number: "1",
-        numberImg: "./public/images/Cocinelle_1.png",
+        numberImg: "./images/Cocinelle_1.png",
         numberText: "One | Un",
     },
     {
         number: "2",
-        numberImg: "./public/images/Cocinelle_2.png",
+        numberImg: "./images/Cocinelle_2.png",
         numberText: "Two | Deux",
     },
     {
         number: "3",
-        numberImg: "./public/images/Cocinelle_3.png",
+        numberImg: "./images/Cocinelle_3.png",
         numberText: "Three | Trois",
     },
     {
         number: "4",
-        numberImg: "./public/images/Cocinelle_4.png",
+        numberImg: "./images/Cocinelle_4.png",
         numberText: "Four | Quatre",
     },
     {
         number: "5",
-        numberImg: "./public/images/Cocinelle_5.png",
+        numberImg: "./images/Cocinelle_5.png",
         numberText: "Five | Cinq",
     },
     {
         number: "6",
-        numberImg: "./public/images/Cocinelle_6.png",
+        numberImg: "./images/Cocinelle_6.png",
         numberText: "Six | Six",
     },
     {
         number: "7",
-        numberImg: "./public/images/Cocinelle_7.png",
+        numberImg: "./images/Cocinelle_7.png",
         numberText: "Seven | Sept",
     },
     {
         number: "8",
-        numberImg: "./public/images/Cocinelle_8.png",
+        numberImg: "./images/Cocinelle_8.png",
         numberText: "Eight | Huit",
     },
     {
         number: "9",
-        numberImg: "./public/images/Cocinelle_9.png",
+        numberImg: "./images/Cocinelle_9.png",
         numberText: "Nine | Neuf",
     },
     {
         number: "10",
-        numberImg: "./public/images/Cocinelle_10.png",
+        numberImg: "./images/Cocinelle_10.png",
         numberText: "Ten | Dix",
     },
     {
         number: "11",
-        numberImg: "./public/images/Cocinelle_11.png",
+        numberImg: "./images/Cocinelle_11.png",
         numberText: "Eleven | Onze",
     },
     {
         number: "12",
-        numberImg: "./public/images/Cocinelle_12.png",
+        numberImg: "./images/Cocinelle_12.png",
         numberText: "Twelve | Douze",
     }
 ];
